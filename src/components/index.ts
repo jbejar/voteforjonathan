f014044e23553071b551baf3b96efa14d57b686a
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2025-04-17 23:13:25 -0600

    Fix build

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 // src/components/index.ts
 
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as Button } from './Button';
+// export { default as Header } from './Header';
+// export { default as Footer } from './Footer';
+// export { default as Button } from './Button';
 // Add more exports as needed for other components
